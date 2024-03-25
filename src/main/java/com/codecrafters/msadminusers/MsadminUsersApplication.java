b8034ac2f96be8bd5d5844ee2814bc127f9c7535
Santicm23 <Santicastrom@outlook.com>
2024-03-25 17:16:36 -0500

    creating springboot project with jwt

---
+++ src/main/java/com/codecrafters/msadminusers/MsadminUsersApplication.java
@@ -0,0 +1,13 @@
+package com.codecrafters.msadminusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsadminUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsadminUsersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codecrafters/msadminusers/MsadminUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecrafters.msadminusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsadminUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
