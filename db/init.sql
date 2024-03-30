6cd77fdf869ede29e5067de2759cbc38adeebd54
Santicm23 <Santicastrom@outlook.com>
2024-03-30 15:08:02 -0500

    minor fix

--- db/init.sql
+++ db/init.sql
@@ -1,2 +1,5 @@
+CREATE SCHEMA IF NOT EXISTS usermicroservice;
+
+
 CREATE USER replicator WITH REPLICATION ENCRYPTED PASSWORD 'replicator_password';
 SELECT pg_create_physical_replication_slot('replication_slot');
