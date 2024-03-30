3ac4397ded062ff97548d725c334bb91455e7cd8
Santicm23 <Santicastrom@outlook.com>
2024-03-30 15:07:23 -0500

    updating docker compose and docker file and removing java files

--- db/init.sql
+++ db/init.sql
@@ -1,5 +1,2 @@
-CREATE SCHEMA IF NOT EXISTS usermicroservice;
-
-
 CREATE USER replicator WITH REPLICATION ENCRYPTED PASSWORD 'replicator_password';
 SELECT pg_create_physical_replication_slot('replication_slot');
