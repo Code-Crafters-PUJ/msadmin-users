c10835b5b2a2105d4e39d7b9653229b94d420693
gams2004 <gabrielmartin2k4@gmail.com>
2024-03-30 23:26:15 -0500

    All hosts allowed

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-&qul_fl9j6-8r0xndmey!ym6shs^q(6a7+28c(0v7%ghp^xadn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '10.147.20.152', '10.147.20.118', '10.147.20.34', '*']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '*']
 
 
 # Application definition
